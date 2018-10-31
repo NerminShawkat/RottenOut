d53628e54aa5158b9111a39e08a94d32fddbae1f
NerminShawkat <shawkat.nermin@gmail.com>
2018-10-31 13:29:57 +0200

    steam v1

---
+++ Assets/Main/Scripts/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    [SerializeField]
+    string _acceptedTag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag(_acceptedTag))
+        {
+            Debug.Log("accepted");
+        }
+    }
+}
